b11bd965b0a56b8a18659e7cdf75fea602cd4a36
trmoro <tristan.moro@outlook.fr>
2020-08-17 12:09:14 +0200

    Correction in circle mesh

--- SoyuzEngine/Meshes/Line.cs
+++ SoyuzEngine/Meshes/Line.cs
@@ -18,7 +18,7 @@ namespace Soyuz.Meshes
 
             for(int i = 0; i < PointNumber; i++)
             {
-                double x = 2 * Math.PI * ((i + 1) / PointNumber);
+                double x = 2 * Math.PI * ((i + 1) / (double) PointNumber);
                 msh.Positions.Add(new Vector3( (float) Math.Cos(x), 0, (float) Math.Sin(x)));
                 msh.Normals.Add(new Vector3(0, 1, 0));
                 msh.UVs.Add(new Vector2( (float)Math.Cos(x), (float)Math.Sin(x) ));
